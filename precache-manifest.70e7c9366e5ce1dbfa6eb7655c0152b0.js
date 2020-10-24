a6d94c9d142aaf09a4177ff1472dc3cf32a40f03
Irtaza Faisal <bcsf19m535@pucit.edu.pk>
2020-10-24 13:52:13 +0500

    Updates

---
+++ precache-manifest.70e7c9366e5ce1dbfa6eb7655c0152b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "032d3bd12872504105c378a7e233f2ab",
+    "url": "/find-me-that/static/media/background_search_mobile.032d3bd1.jpg"
+  },
+  {
+    "revision": "3b3bddbe00936ba281948c4c270ce20f",
+    "url": "/find-me-that/static/media/background_search_desktop.3b3bddbe.jpg"
+  },
+  {
+    "revision": "60b7df4be12a38234226",
+    "url": "/find-me-that/static/js/runtime~main.66e60770.js"
+  },
+  {
+    "revision": "c8dde0d770516042aab9",
+    "url": "/find-me-that/static/js/main.f5980e00.chunk.js"
+  },
+  {
+    "revision": "4fccef06ced851d2dab2",
+    "url": "/find-me-that/static/js/2.241e511d.chunk.js"
+  },
+  {
+    "revision": "c8dde0d770516042aab9",
+    "url": "/find-me-that/static/css/main.803478cc.chunk.css"
+  },
+  {
+    "revision": "6a293cad3be3904af44d6823a7610b50",
+    "url": "/find-me-that/index.html"
+  }
+];
